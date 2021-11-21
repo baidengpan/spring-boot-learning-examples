3ba36cf5674ef30d12b50c605d0272a371d1c1bf
baidengpan <953127719@qq.com>
2021-11-21 19:27:48 +0800

    test change http code

---
+++ spring-boot-quickstart/src/main/java/com/rhwayfun/springboot/quickstart/web/BadRequest.java
@@ -0,0 +1,23 @@
+package com.rhwayfun.springboot.quickstart.web;
+
+import com.alibaba.fastjson.JSONObject;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by chubin on 2017/9/10.
+ */
+@RestController
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class BadRequest {
+
+    @RequestMapping("/test1")
+    public String test(){
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("key1", "value1");
+        return jsonObject.toString();
+    }
+
+}
