d30784348080cb9395b856d3b18307b4c9a97c6e
baidengpan <baidengpan@meituan.com>
2022-01-07 21:00:31 +0800

    test

--- spring-boot-quickstart/src/main/java/com/rhwayfun/springboot/quickstart/web/BadRequest.java
+++ spring-boot-quickstart/src/main/java/com/rhwayfun/springboot/quickstart/web/BadRequest.java
@@ -6,9 +6,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-/**
- * Created by chubin on 2017/9/10.
- */
 @RestController
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class BadRequest {
--- spring-boot-quickstart/src/main/java/com/rhwayfun/springboot/quickstart/web/DemoController.java
+++ spring-boot-quickstart/src/main/java/com/rhwayfun/springboot/quickstart/web/DemoController.java
@@ -1,7 +1,7 @@
 package com.rhwayfun.springboot.quickstart.web;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.alibaba.fastjson.JSON;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Created by chubin on 2017/9/10.
@@ -10,7 +10,21 @@ import org.springframework.web.bind.annotation.RestController;
 public class DemoController {
 
     @RequestMapping("/now")
-    public String now(){
+    public String now(String param1){
+        System.out.println("param1:" + param1);
         return "hello";
+    }
+
+    @PostMapping("/post")
+    public String post(@RequestParam String key, @RequestParam String value) {
+        System.out.println("key:" + key + ",value:" + value);
+        return key + ":" + value;
+    }
+
+    @PostMapping("/post1")
+    public String post1(@RequestBody String body) {
+        System.out.println("body:" + body);
+        JSON json = JSON.parseObject(body);
+        return body;
     }
 }
